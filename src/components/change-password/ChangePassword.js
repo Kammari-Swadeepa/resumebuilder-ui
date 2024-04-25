712ac4d238088aebf7c4413e5b4689447ff596bf
shiva2925 <shivavisualpath29@gmail.com>
2024-04-25 11:44:06 +0530

    latest

---
+++ src/components/change-password/ChangePassword.js
@@ -0,0 +1,242 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { PostApi } from '../../services/commonServices.js';
+function ChangePassword() {
+    const [isToast, setIsToast] = useState(false)
+    const [status, setStatus] = useState(null)
+    const [message,setMessage] = useState("");
+    const [password, setPassword] = useState({
+        oldpassword: "",
+        userid: "",
+        newpassword: "",
+        verifypassword: ""
+    })
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setPassword({ ...password, [name]: value })
+
+    }
+    const handleChangePassword =async (e) => {
+        e.preventDefault()
+        if (password.oldpassword == '') {
+            toast.error('Current password field should not be empty', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+           
+        }
+         else if (password.newpassword == '') {
+            toast.error('New password field should not be empty"', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+        
+        }
+        else if (password.verifypassword == '') {
+            toast.error('Confirm password field should not be empty"', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+         
+        }
+         else if (password.newpassword != password.verifypassword) {
+            toast.error('New password, Confirm password should be match"', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+       
+        } else{
+    var tntId= JSON.parse(localStorage.getItem('tID'))
+
+            const sessiondetails =  localStorage.getItem(`userdata${tntId}`);
+      if (sessiondetails != null) {
+        const userdata = JSON.parse(sessiondetails);
+        // console.log(userdata, "userdata")
+       
+            const respdata ={
+              oldpassword:password.oldpassword,
+              userid: userdata.id,
+              newpassword:password.newpassword
+      
+            }
+            const ResponseMessage = await PostApi(respdata,'CHANGEPASSWORD');
+            // console.log(ResponseMessage,"ResponseMessage")
+            if(ResponseMessage.data =='USER_NOT_FOUND'){
+                toast.error('User not found', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+             
+            
+            }else if(ResponseMessage.data =='INVALIDPASSWORD'){
+                toast.error('Invalid old password', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+             
+            }else if(ResponseMessage.data =='INACTIVE'){
+                toast.error('Your account is inactive ,Please contact admin', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+      
+            }
+           else if(ResponseMessage.data =='ALREADY_LOGGEDIN'){
+            toast.error('You have already logged in with this mobile number , Please kindly logout or contact support team', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+      
+             
+            }
+            else{
+                toast.success('Password changed successfully, Please Login again, Redirecting to Home ....', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+              
+                setTimeout(async()=>{
+                    setIsToast(false)
+                    localStorage.removeItem("userdata")
+                   
+
+                    const reqparams = {
+                        mobileno: userdata.user.mobileno
+                        
+            
+                    }
+                    const clearsessionrespone = await PostApi(reqparams, 'LOGOUT');
+                    // console.log("clearsessionrespone", clearsessionrespone);
+                    var tntId=JSON.parse(localStorage.getItem('tID'))
+            
+                    localStorage.removeItem(`userdata${tntId}`)
+                    window.location.href = '/'
+                },5000)
+      
+          }
+           
+          }
+    }}
+  return (
+    <>
+     
+        <div>
+     
+     <form  id="main_login_form" novalidate="">
+           <div class="row">
+            
+              <div class="col-12 col-md-12">
+                   <div class="group">
+                       <input type="password" id="current_password" name="oldpassword"
+                                            value={password.oldpassword}
+                                            onChange={handleChange}
+                                            style={{letterSpacing:"2px"}}/>
+                       <span class="highlight"></span>
+                       <span class="bar"></span>
+                       <label>Current Password</label>
+                   </div>
+               </div>
+               <div class="col-12 col-md-12">
+                   <div class="group">
+                       <input type="password" id="new_password" name="newpassword"
+                                            value={password.newpassword}
+                                            onChange={handleChange}
+                                            style={{letterSpacing:"2px"}}/>
+                       <span class="highlight"></span>
+                       <span class="bar"></span>
+                       <label>New Password</label>
+                   </div>
+               </div>
+               <div class="col-12 col-md-12">
+                   <div class="group">
+                       <input type="password" id="confirm_password" name="verifypassword"
+                                            value={password.verifypassword}
+                                            onChange={handleChange}
+                                            style={{letterSpacing:"2px"}}/>
+                       <span class="highlight"></span>
+                       <span class="bar"></span>
+                       <label>Confirm Password</label>
+                   </div>
+               </div>
+               
+               
+               <div class="col-12 col-sm-5 text-left ">
+                   <button type="submit" class="btn dream-btn" onClick={handleChangePassword}>Change Password</button>
+               </div>
+               
+             
+           </div>
+       </form>
+ 
+       <div class="other-accounts text-center">
+           <p class="w-text">Login with other account</p>
+           <div class="footer-social-info">
+               <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+               <a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+               <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+           </div>
+       </div>
+   </div>
+    </>
+
+  )
+}
+
+export default ChangePassword
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,23 @@
 import React, { Suspense, useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { BsShieldLock } from "react-icons/bs";
+import { CiCircleMore } from "react-icons/ci";
+import { PiSignOutBold } from "react-icons/pi";
 import { RxCross1 } from "react-icons/rx";
 import { Link } from 'react-router-dom';
 import logo from "../../assests/img/core-img/logo.png";
+import { PostApi } from '../../services/commonServices.js';
+import ChangePassword from '../change-password/ChangePassword.js';
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
-import { GetApi,PostApi } from '../../services/commonServices.js';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { PiSignOutBold } from "react-icons/pi";
-import { CiCircleMore } from "react-icons/ci";
-import { BsShieldLock } from "react-icons/bs";
 function Header() {
 
   const [loginModal, setLoginModal] = useState(false)
   const [signupModal, setSignupModal] = useState(false)
   const [user, setUser] = useState(null);
   const [userMobile, setUserMobile] = useState("")
+  const [isChangePassword,setIsChangePassword]=useState(false)
   useEffect(()=>{
     var tntId = JSON.parse(localStorage.getItem('tID'))
 
@@ -54,6 +56,9 @@ function Header() {
   }
   const changePassword =()=>{
     console.log("password change")
+  }
+  const handleChangePassword = ()=>{
+    setIsChangePassword(true)
   }
   return (
     <>
@@ -98,17 +103,10 @@ function Header() {
   <Dropdown.Toggle variant="muted" id="dropdown-basic"  style={{border:"none",backgroundColor:"transparent",outline:"none"}}>
   <CiCircleMore className='mt-2 ' style={{border:"none",width:"27px", height:"27px", borderRadius:"50%",outline:"none"}} />
                                 </Dropdown.Toggle>
-                                <Dropdown.Menu style={{width:'270px'}}>
-                                    
-
-
-
+                                <Dropdown.Menu style={{width:'230px'}}>
                                    
-
-
-                                    <Dropdown.Item onClick={LoginOutFunc} style={{ fontSize: "18px", padding: "10px" }}><PiSignOutBold style={{ fontSize: "26px", margin: "0px 10px" }} /><span style={{position:'relative',bottom:"6px"}}>Sign Out</span></Dropdown.Item>
-                                    <Dropdown.Item onClick={changePassword} style={{ fontSize: "18px", padding: "10px" }}><BsShieldLock style={{ fontSize: "26px", margin: "0px 10px" }} /><span style={{position:'relative',bottom:"6px"}}>Change Password</span></Dropdown.Item>
-
+                                    <Dropdown.Item onClick={changePassword} style={{ fontSize: "16px", padding: "10px" }}><BsShieldLock style={{ fontSize: "21px", margin: "0px 10px" }} /><span style={{position:'relative',bottom:"4px"}} onClick={handleChangePassword}>Change Password</span></Dropdown.Item>
+                                    <Dropdown.Item onClick={LoginOutFunc} style={{ fontSize: "16px", padding: "10px" }}><PiSignOutBold style={{ fontSize: "21px", margin: "0px 10px" }} /><span style={{position:'relative',bottom:"4px"}}>Sign Out</span></Dropdown.Item>
                                 </Dropdown.Menu>
                             </Dropdown>: <><a onClick={LoginFnc} class="open-popup-link btn login-btn mr-im">Sign In</a>
                   <a onClick={SignupFnc} class="open-signup-link btn login-btn">Sign Up </a></> }
@@ -155,6 +153,32 @@ function Header() {
                 <Suspense>
                   <Signup/>
                   
+                </Suspense>
+                
+              </div>
+
+            </form>
+
+          </div>
+        </div>
+      </Modal>
+
+
+
+
+
+      <Modal className="modal fade" size="md" show={isChangePassword} onHide={()=>{setIsChangePassword(false)}} style={{marginTop:"70px"}}>
+        <div className="" role="document">
+          <div className="">
+            <form >
+            <div className="modal-header border-none" style={{position:"relative"}} >
+              <h4>Change Password</h4>
+<RxCross1 style={{fontSize:"26px", position:"absolute", right:"10px"}} onClick={() => setIsChangePassword(false)} />
+                {/* <button type="button" className="btn-close border-none" onClick={() => setSignupModal(false)} data-dismiss="modal">X</button> */}
+              </div>
+              <div className="modal-body">
+                <Suspense>
+                <ChangePassword />
                 </Suspense>
                 
               </div>
